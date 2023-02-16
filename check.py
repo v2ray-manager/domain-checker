4e9ba7316ffc7f25ab9bacfcb47ec719638f7536
Farbod <cppoxking@gmail.com>
2023-02-15 22:08:36 -0800

    Update check.py

--- check.py
+++ check.py
@@ -1,4 +1,13 @@
-import requests
+import requests,os,json,sys
+    
+def send(bot_message):
+       bot_token = os.environ.get("TOKEN")
+       bot_chatID = os.environ.get("CHATID")
+       send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&disable_web_page_preview=true&text=' + bot_message
+    
+       response = requests.get(send_text)
+    
+       return response.json()
 
 x = requests.post('https://check-host.net/check-http?host=albalo.store:443&node=ir4.node.check-host.net&node=ir3.node.check-host.net&node=ir1.node.check-host.net',headers={'Accept': 'application/json'})
-print(x.json())
+send(x.json()["request_id"])
