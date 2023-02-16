350fa00b235b51a7606546a8f056c50cc91e0876
Farbod <cppoxking@gmail.com>
2023-02-15 22:38:21 -0800

    Update check.py

--- check.py
+++ check.py
@@ -10,4 +10,4 @@ def send(bot_message):
        return response.json()
 
 x = requests.post('https://check-host.net/check-http?host=albalo.store:443&node=ir4.node.check-host.net&node=ir3.node.check-host.net&node=ir1.node.check-host.net',headers={'Accept': 'application/json'})
-send(x.json()["request_id"])
+send(x.text)
